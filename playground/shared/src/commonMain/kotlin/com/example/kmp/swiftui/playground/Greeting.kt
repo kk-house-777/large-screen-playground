82c9ac4e244f96ebae95816f5eefbf8a7ab79e6e
kk-777-1127 <kk__777@hatena.ne.jp>
2025-09-21 08:50:13 +0900

    reset

--- playground/shared/src/commonMain/kotlin/com/example/kmp/swiftui/playground/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kmp.swiftui.playground
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- playground/shared/src/commonMain/kotlin/com/example/kmp/swiftui/playground/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kmp.swiftui.playground
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
